3d407a2f1f49777fbb433aacf4e4c4f6f4afb9b5
marci <marcifey@gmail.com>
2018-08-04 20:57:48 -0700

    changes to package.json

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,7 @@ module.exports = {
   entry: './src/app.js',
   output: {
     path: path.join(__dirname, 'public'),
-    filename: 'bundle.js',
-    publicPath: '/public/'
+    filename: 'bundle.js'
   },
   node: {
      fs: "empty"
